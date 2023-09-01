a0bc274453944c851f02ebd9c8e0ea80b633b89d
RyumaKazue <yamaguchi-u.ac.jp>
2023-09-01 13:27:11 +0900

    update

--- memo.py
+++ memo.py
@@ -2,13 +2,19 @@ import streamlit as st
 import pandas as pd
 import sqlite3 as sqlite
 
+def addTaskToDatabase(db, task, limit):
+    if not task:
+        st.error('タスクの名前が記入されていません')
+    db.execute(f"""
+        INSERT INTO memo_db(task, limit_date) VALUES('{task}', '{limit}')""")
+    db.commit()
+
 st.title("課題メモアプリ")
 
 #データベースにアクセス
 db = sqlite.connect('memo_db')
 #データベースにmemo_dbがない場合は作成する
 db.execute("CREATE TABLE IF NOT EXISTS memo_db ( id INTEGER PRIMARY KEY, task TEXT NOT NULL, limit_date TEXT NOT NULL)")
-db.commit()
 
 #memo_dbからデータを取得
 cursor = db.cursor()
@@ -26,15 +32,12 @@ df = pd.DataFrame(data={
     'Limit':limit_dates
 })
 
+
 st.write('タスク管理表', df)
 
 task = st.text_input('課題')
 limit = st.date_input('期限')
 
-if st.button('追加する'):
-    if not task:
-        st.error('タスクの名前が記入されていません')
-    else:
-        db.execute(f"""
-        INSERT INTO memo_db(task, limit_date) VALUES('{task}', '{limit}')""")
-        db.commit()
+st.button('追加する', on_click=addTaskToDatabase(db, task, limit))
+    
+
